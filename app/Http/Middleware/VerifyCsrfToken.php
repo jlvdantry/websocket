cac9efdfb201187e9663bb1a69732bad97cd97eb
Guillermo Hernandez <underdog1987@yandex.ru>
2020-12-02 00:32:30 -0600

    chore:Log Token csrf

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -3,6 +3,7 @@
 namespace App\Http\Middleware;
 
 use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken as Middleware;
+use App\AdipUtils\ErrorLoggerService as Logg;
 
 class VerifyCsrfToken extends Middleware
 {
@@ -14,4 +15,31 @@ class VerifyCsrfToken extends Middleware
     protected $except = [
         //
     ];
+
+
+
+
+    /**
+     * Determine if the session and input CSRF tokens match.
+     *
+     * @override
+     * @param  \Illuminate\Http\Request  $request
+     * @return bool
+     */
+    protected function tokensMatch($request)
+    {
+        $token = $this->getTokenFromRequest($request);
+
+        $ret = is_string($request->session()->token()) &&
+               is_string($token) &&
+               hash_equals($request->session()->token(), $token);
+        if(!$ret){
+            try{
+                Logg::log(__METHOD__,'Token no coincide. Se esperaba '.$request->session()->token().' pero se recibió '.$token );
+            }catch(\Exception $e){
+                
+            }
+        }
+        return $ret;
+    }
 }
